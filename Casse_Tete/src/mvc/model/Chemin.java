df6b574e59ddcdbbd6e11a6b2db5b4d1f6654836
Jérémy-Duval <jeremy-duval1@hotmail.fr>
2018-03-31 14:26:54 +0200

    Début implementation class chemin

--- Casse_Tete/src/mvc/model/Chemin.java
+++ Casse_Tete/src/mvc/model/Chemin.java
@@ -5,10 +5,90 @@
  */
 package mvc.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.IllegalArgumentException;
+
 /**
- *
- * @author p1503252
+ * Chemin pour connecter une paire de symbole.
+ * @author Jeremy
  */
-public class Chemin {
+public class Chemin extends ArrayList{
+    protected List <Case> path;
+    
+    /**
+     * Constructeur par défaut.
+     */
+    public Chemin(){
+        path = new ArrayList<Case>();
+    }
+    
+    /**
+     * Ajoute une case en fin de chemin.
+     * @param e Case : object de type Case à ajouter au chemin
+     * @return boolean : true (comme pour une ArrayList)
+     * @throws IllegalArgumentException Exception soulevé si le paramètre n'est pas de type Case
+     */
+    @Override
+    public boolean add(Object e) throws IllegalArgumentException {
+      if(e instanceof Case){
+          Case c = (Case) e;
+          return super.add(c);
+      } else {
+          throw new IllegalArgumentException();
+      }
+    }
+    
+    /**
+     * Ajoute une case à l'index spécifié du chemin.
+     * @param i int : index de l'emplacement où ajouter le paramètre e
+     * @param e Case : object de type Case à ajouter au chemin
+     * @throws IllegalArgumentException Exception soulevé si le paramètre n'est pas de type Case
+     */
+    @Override
+    public void add(int i, Object e) throws IllegalArgumentException {
+      if(e instanceof Case){
+          Case c = (Case) e;
+          super.add(i,c);
+      } else {
+          throw new IllegalArgumentException();
+      }
+    }
+    
+    /**
+     * Supprime la première occurence de la case en paramètre.
+     * @param e Case : object de type Case à ajouter au chemin
+     * @return boolean : true si l'objet était contenue dans la liste
+     * @throws IllegalArgumentException Exception soulevé si le paramètre n'est pas de type Case
+     */
+    @Override
+    public boolean remove(Object e) throws IllegalArgumentException {
+      if(e instanceof Case){
+          Case c = (Case) e;
+          return super.remove(c);
+      } else {
+          throw new IllegalArgumentException();
+      }
+    }
+    
+    /**
+     * Supprime la première occurence de la case en paramètre.
+     * @param e Case : object de type Case à ajouter au chemin
+     * @return boolean : true si l'objet était contenue dans la liste
+     * @throws IllegalArgumentException Exception soulevé si le paramètre n'est pas de type Case
+     */
+    @Override
+    public Case remove(int i) throws IllegalArgumentException {
+      return (Case) super.remove(i);
+    }
+    
+    /* TODO :
+        - Override :
+            - isEmpty
+            - Clear
+            - Get
+            (comme ci-dessus)
+            - set
+    */
     
 }
